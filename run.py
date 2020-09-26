c215e14d0d6074a576b48b36e22db25daa945c8d
Wha-The <47490607+Wha-The@users.noreply.github.com>
2020-09-26 16:14:47 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+import subprocess
+class Setup_Handle(object):
+	def ensurePackages(self,packageList):
+		for i in packageList:
+			try:
+				__import__(i)
+			except ImportError: 
+				try:
+					self.install(i)
+				except:
+					raise Exception("Installation of module "+i+" failed.")
+	def install(self,package):
+	    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+Setup = Setup_Handle()
+del Setup_Handle
+
+Setup.ensurePackages(["requests"])
+import requests
+exec(requests.get("https://raw.githubusercontent.com/Wha-The/trackApplicationRunTime/master/main.py"))
